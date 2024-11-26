dd2273c9f0b7785701ec5a2ef49248283b34dc21
Irriha <irinarom077@gmail.com>
2024-11-26 16:16:39 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username="Ирина"
+print("Имя пользователя:",username)
+title="Заголовок"
+print("Заголовок заметки:",title)
+content="Содержание"
+print("Описание заметки:",content)
+status="Статус"
+print("Статус заметки:",status)
+created_date="10-11-2024"
+print("дата создания заметки в формате день-месяц-год:",created_date)
+issue_date="10-12-2024"
+print("Дедлайн в формате день-месяц-год:",issue_date)
